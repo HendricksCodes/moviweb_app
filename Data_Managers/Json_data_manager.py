dab5851b5b288800394f234a4eb3f7804f375d32
HendricksCodes <natejh1124@gmail.com>
2023-07-02 10:57:07 -0700

    sorted the files in pycharm and implemented methods for Json_data_manager.py and csv_data_manager.py

--- Data_Managers/Json_data_manager.py
+++ Data_Managers/Json_data_manager.py
@@ -1,15 +1,92 @@
 import json
-from .data_manager_interface import DataManagerInterface
+from data_manager_interface import DataManagerInterface
 
 
 class JSONDataManager(DataManagerInterface):
     def __init__(self, filename):
         self.filename = filename
+        self.data = self.load_data()
+
+    def load_data(self):
+        with open(self.filename, 'r') as file:
+            data = json.load(file)
+        return data
+
+    def save_data(self):
+        with open(self.filename, 'w') as file:
+            json.dump(self.data, file, indent=2)
 
     def get_all_users(self):
-        # Return a list of all users
-        pass
+        return self.data
 
     def get_user_movies(self, user_id):
-        # Return a list of all movies for a given user
-        pass
+        for user in self.data:
+            if user['id'] == user_id:
+                return user.get('movies', [])
+        return []
+
+    def add_user(self, name):
+        user_id = max(user['id'] for user in self.data) + 1 if self.data else 1
+        user = {
+            'id': user_id,
+            'name': name,
+            'movies': []
+        }
+        self.data.append(user)
+        self.save_data()
+        return user_id
+
+    def add_movie(self, user_id, name, director, year, rating):
+        user = self.get_user_by_id(user_id)
+        if user:
+            movie_id = max(movie['id'] for movie in user['movies']) + 1 if user['movies'] else 1
+            movie = {
+                'id': movie_id,
+                'name': name,
+                'director': director,
+                'year': year,
+                'rating': rating
+            }
+            user['movies'].append(movie)
+            self.save_data()
+            return movie_id
+        return None
+
+    def update_movie(self, user_id, movie_id, name=None, director=None, year=None, rating=None):
+        movie = self.get_movie_by_id(user_id, movie_id)
+        if movie:
+            if name:
+                movie['name'] = name
+            if director:
+                movie['director'] = director
+            if year:
+                movie['year'] = year
+            if rating:
+                movie['rating'] = rating
+            self.save_data()
+            return True
+        return False
+
+    def remove_movie(self, user_id, movie_id):
+        user = self.get_user_by_id(user_id)
+        if user:
+            for movie in user['movies']:
+                if movie['id'] == movie_id:
+                    user['movies'].remove(movie)
+                    self.save_data()
+                    return True
+        return False
+
+    def get_user_by_id(self, user_id):
+        for user in self.data:
+            if user['id'] == user_id:
+                return user
+        return None
+
+    def get_movie_by_id(self, user_id, movie_id):
+        user = self.get_user_by_id(user_id)
+        if user:
+            for movie in user['movies']:
+                if movie['id'] == movie_id:
+                    return movie
+        return None
--- Data_Managers/csv_data_manager.py
+++ Data_Managers/csv_data_manager.py
@@ -7,9 +7,91 @@ class CSVDataManager(DataManagerInterface):
         self.filename = filename
 
     def get_all_users(self):
-        # Return a list of all users
-        pass
+        users = []
+        with open(self.filename, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                users.append({
+                    'id': int(row['id']),
+                    'name': row['name'],
+                    'movies': []
+                })
+        return users
 
     def get_user_movies(self, user_id):
-        # Return a list of all movies
-        pass
+        movies = []
+        with open(self.filename, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if int(row['id']) == user_id:
+                    movies.append({
+                        'id': int(row['movie_id']),
+                        'name': row['movie_name'],
+                        'director': row['director'],
+                        'year': int(row['year']),
+                        'rating': float(row['rating'])
+                    })
+        return movies
+
+    def add_user(self, name):
+        users = self.get_all_users()
+        user_id = max(user['id'] for user in users) + 1 if users else 1
+
+        with open(self.filename, 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([user_id, name])
+        return user_id
+
+    def add_movie(self, user_id, name, director, year, rating):
+        movies = self.get_user_movies(user_id)
+        movie_id = max(movie['id'] for movie in movies) + 1 if movies else 1
+
+        with open(self.filename, 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([user_id, movie_id, name, director, year, rating])
+        return movie_id
+
+    def update_movie(self, user_id, movie_id, name=None, director=None, year=None, rating=None):
+        updated = False
+        rows = []
+
+        with open(self.filename, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if int(row[0]) == user_id and int(row[1]) == movie_id:
+                    if name:
+                        row[2] = name
+                    if director:
+                        row[3] = director
+                    if year:
+                        row[4] = year
+                    if rating:
+                        row[5] = rating
+                    updated = True
+                rows.append(row)
+
+        if updated:
+            with open(self.filename, 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerows(rows)
+
+        return updated
+
+    def remove_movie(self, user_id, movie_id):
+        removed = False
+        rows = []
+
+        with open(self.filename, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if int(row[0]) == user_id and int(row[1]) == movie_id:
+                    removed = True
+                else:
+                    rows.append(row)
+
+        if removed:
+            with open(self.filename, 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerows(rows)
+
+        return removed
